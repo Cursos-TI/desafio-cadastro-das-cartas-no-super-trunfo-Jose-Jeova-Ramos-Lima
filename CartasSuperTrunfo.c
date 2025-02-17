068e9b91687ea9acfc371978a515717961bf7534
Jose-Jeova-Ramos-Lima <jeovarlima@yahoo.com.br>
2025-02-17 19:47:13 +0000

    Corrigi erros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,29 +5,36 @@
 int main() {
     printf("Desafio Super Trunfo - Novato \n"); // saída de dados que mostra na tela o título do desafio
 
-    char nome[200] = "Gotham City do Batman"; // declaração de variável do nome da cidade, tipo string
-    int população = 100000000000; // declaração de variável da população, tipo inteiro
-    double área = 12000000.8; // declaração de variável da área, tipo flutuante
-    float PIB = 500.8; // declaração de variável do PIB, tipo flutuante
-    int pontos_turísticos = 2500000000000000; // declaração de variável dos pontos turíticos, tipo inteiro
-
+    char nome[50] = "Gotham City do Batman"; // declaração de variável do nome da cidade, tipo string
+    int populacao = 1000000; // declaração de variável da população, tipo inteiro
+    double area = 12000.8; // declaração de variável da área, tipo flutuante
+    double PIB = 500.8; // declaração de variável do PIB, tipo flutuante
+    int pontos_turisticos = 2500000000; // declaração de variável dos pontos turíticos, tipo inteiro
+    
+    printf("Digite o nome da cidade: \n"); // saída de dados que mostra na tela a mensagem para o usuário inserir o nome da cidade
+    scanf ("%s", &nome); // entrada de dados do nome da cidade inserida pelo usuário
+    
     printf("Digite a população da cidade: \n"); // saída de dados que mostra na tela a mensagem para o usuário inserir a população
-    scanf ("%d", &população); // entrada de dados da população inserida pelo usuário
-
+    scanf ("%d", &populacao); // entrada de dados da população inserida pelo usuário
+    
     printf("Digite a área da cidade em km²: \n"); // saída de dados que mostra na tela a mensagem para o usuário inserir a área
-    scanf ("%f", &área); // entrada de dados da área inserida pelo usuário
-
+    scanf ("%lf", &area); // entrada de dados da área inserida pelo usuário
+    
     printf("Digite o PIB da cidade em bilhões de reais: \n"); // saída de dados que mostra na tela a mensagem para o usuário inserir o PIB
-    scanf ("%f", &PIB); // entrada de dados do PIB inserido pelo usuário
-
+    scanf ("%lf", &PIB); // entrada de dados do PIB inserido pelo usuário
+    
     printf("Digite o número de pontos turísticos da cidade: \n"); // saída de dados que mostra na tela a mensagem para o usuário inserir o número de pontos turísticos
-    scanf ("%d", &pontos_turísticos); // entrada de dados do número de pontos turísticos inserido pelo usuário
-
+    scanf ("%d", &pontos_turisticos); // entrada de dados do número de pontos turísticos inserido pelo usuário
+    
     printf("Cidade: %s\n", nome); // saída de dados que mostra o nome da cidade inserido pelo usuário
-    printf("População: %d\ n", população); // saída de dados que mostra a população insderida pelo usuário
-    printf("Área: %f km²\n", área); // saída de dados que mostra a área inserida pelo usuário
-    printf("PIB: %f bilhões de reais\n", PIB); // saída de dados que mostra o PIB inserido pelo usuário) 
-    printf("Pontos turísticos: %d\n", pontos_turísticos); // saída de dados que mostra o número de pontos turísticos inserido pelo usuário
+    
+    printf("População: %d\n", populacao); // saída de dados que mostra a população insderida pelo usuário
+    
+    printf("Área: %lf km²\n", area); // saída de dados que mostra a área inserida pelo usuário
+    
+    printf("PIB: %lf bilhões de reais\n", PIB); // saída de dados que mostra o PIB inserido pelo usuário) 
+    
+    printf("Pontos turísticos: %d\n", pontos_turisticos); // saída de dados que mostra o número de pontos turísticos inserido pelo usuário
 
     
     return 0;
